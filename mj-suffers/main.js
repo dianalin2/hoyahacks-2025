b02d6497aecb95c738c7f4cb4f602629a33f41ae
Diana Lin <lindiana1206@gmail.com>
2025-01-25 06:49:18 -0500

    add api to access file index

--- mj-suffers/main.js
+++ mj-suffers/main.js
@@ -1,9 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      autoplay: false,
+      autofill: false,
+      preload: path.join(__dirname, 'preload.js'),
+    }
   })
 
   win.loadFile('index.html')
@@ -11,4 +18,26 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
-})      
+
+  ipcMain.on('queryAll', async (event) => {
+    try {
+      const response = await fetch('http://localhost:5000/all');
+      const files = await response.text();
+      event.sender.send('queryAll-success', files)
+    } catch (err) {
+      console.error(err)
+      event.sender.send('queryAll-error', err)
+    }
+  });
+
+  ipcMain.on('queryDB', async (event) => {
+    try {
+      const response = await fetch('http://localhost:5000/query');
+      const files = await response.json();
+      event.sender.send('queryDB-success', files)
+    } catch (err) {
+      console.error(err)
+      event.sender.send('queryDB-error', err)
+    }
+  });
+});
